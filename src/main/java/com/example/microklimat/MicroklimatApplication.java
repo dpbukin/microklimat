00f1df2a045c3739d1d86056c3cd919d44d2237a
dpbukin <d.bukin.dev@yandex.ru>
2024-12-16 02:54:01 +0300

    init

---
+++ src/main/java/com/example/microklimat/MicroklimatApplication.java
@@ -0,0 +1,13 @@
+package com.example.microklimat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroklimatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroklimatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/microklimat/MicroklimatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microklimat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroklimatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
